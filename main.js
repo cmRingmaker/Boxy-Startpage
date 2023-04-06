2db343c3cf5f777d647f0123de8af96f3a68314a
ringmaker <chrisringmaker@gmail.com>
2023-04-06 01:08:41 -0400

    basic layout draft with HTML and CSS

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('hello world!')
+// function to create a new div in order to add a new "container" to the hero section
+
+// function to ask user input to add links within the new containers
+
+// add localstorage key value pairs to save hero section info
