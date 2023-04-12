7c0244bbea09fc96cab3020964526d02e72cd56d
ringmaker <chrisringmaker@gmail.com>
2023-04-12 01:09:19 -0400

    buttons can now be toggled, changed theme colors

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // TODO :
 // function to create a new div in order to add a new "container" to the hero section
-
 // function to ask user input to add links within the new containers
-
-// add localstorage key value pairs to save hero section info
+// function to add tasks to the task list
+// function to save current state of selected button/form
+// function to save to localstorage for links, forms, and tasks
 // -----------------------
 
 const form = document.getElementById('formInput')
@@ -11,7 +11,9 @@ const search = document.getElementById('searchInput')
 const searchToggle = document.querySelector('#searchBtn')
 const linksToggle = document.querySelector('#linksBtn')
 const todoToggle = document.querySelector('#todoBtn')
-const svg = document.querySelectorAll('.svgBtn')
+const toggleButton = document.querySelectorAll('.svgBtn')
+const todos = document.querySelector('#todos')
+
 
 // Clock
 function currentTime() {
@@ -29,19 +31,20 @@ function currentTime() {
 
   let time = `${hh}:${mm} ${ampm}`
   document.getElementById("clock").innerText = time
-  //since there are no seconds being tracked, the clock is only updated every 20sec instead of 1sec for slight performance boost
+  //since there are no seconds being tracked, the clock is only updated every 20sec instead of 1sec for slight performance
   setTimeout(currentTime, 20000)
 }
+
+currentTime() // call
 
 //using commands such as /r /y /t ...etc the user can search directly on a given site
 function searchFunc() {
   const command = search.value.slice(0, 3).toLowerCase()
   const actionInput = search.value.slice(3)
+  const joinStr = actionInput.split(' ').join('') // make input into one word if it has spaces
 
   switch (command) {
     case '/r ': //reddit
-      // make input into one word if it has spaces. ... e.g. /r start pages = /r startpages
-      const joinStr = actionInput.split(' ').join('')
       form.action = `https://reddit.com/r/${joinStr}`
       break;
 
@@ -50,17 +53,16 @@ function searchFunc() {
       form.method = 'POST'
       break;
 
-    case '/t ': //twitter
-      // search hashtags specifically or just a general search for the keyword
+    case '/t ': //twitter -- search hashtags specifically or just a general search for the keyword
       form.action = (actionInput.includes('#'))
-                    ? `https://twitter.com/hashtag/${actionInput.slice(1)}` 
+                    ? `https://twitter.com/hashtag/${joinStr.slice(1)}` 
                     : `https://twitter.com/search/${actionInput}`
       break;
 
     case '/s ': //stack overflow
       form.action = `https://stackoverflow.com/search?q=${actionInput}`
       form.method = 'POST'
-      break; 
+      break;
 
     case '/d ': //duck duck go
       form.action = `https://duckduckgo.com/${actionInput}`
@@ -69,8 +71,52 @@ function searchFunc() {
     // TODO : allow user to choose their default search
 
     default: // by default we are set to google on the html form
+      form.action = `https://google.com/search?${actionInput}`
       search.name = 'q'
   }
 }
 
-// TODO: add functionality to select icon above search bar to determine whether to use search, add a link, or add a task
+
+// |-----------------------------------|
+// | * * PAGE AND FORM STATE BEGIN * * |
+// |-----------------------------------|
+
+// Set active to last clicked button, call inputSaveState() and get data from localStorage
+toggleButton.forEach((btn) => {
+  btn.addEventListener('click', e => {
+    toggleButton.forEach((b) => b.classList.remove('active'))
+    btn.classList.add('active')
+    inputSaveState()
+  })
+})
+
+function inputSaveState() {
+  // Save current state of selected button above form and save to localStorage
+  // e.g. click search -> add 'active' to search class -> change form state to 'search' parameters
+  const LOCAL_STORAGE_SAVESTATE_KEY = 'current.activeForm'
+  let activeForm = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SAVESTATE_KEY)) || []
+
+  switch(true) {
+    // CONTROLS SEARCH TO INTERNET
+    case searchToggle.classList.contains('active'):
+      console.log('search active') // remove later
+      search.placeholder = 'Searchin...'
+      break;
+
+    // CONTROLS ADDING LINKS TO PAGE
+    case linksToggle.classList.contains('active'):
+      console.log('links active') // remove later
+      search.placeholder = 'Add a Link...'
+      break;
+
+    // CONTROLS ADDING TASKS TO TASKLIST
+    case todoToggle.classList.contains('active'):
+      console.log('todo active') // remove later
+      search.placeholder = 'Add a Task...'
+    break;
+  }
+}
+
+// |---------------------------------|
+// | * * PAGE AND FORM STATE END * * |
+// |---------------------------------|
