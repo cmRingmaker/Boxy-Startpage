bf5ff8c69671242be5ee15e5df08e9770c4ca456
ringmaker <chrisringmaker@gmail.com>
2023-04-15 02:44:58 -0400

    new buttons, localstorage working, forms working

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-// TODO :
-// function to create a new div in order to add a new "container" to the hero section
-// function to ask user input to add links within the new containers
-// function to add tasks to the task list
-// function to save current state of selected button/form
-// function to save to localstorage for links, forms, and tasks
-// -----------------------
-
-const form = document.getElementById('formInput')
-const search = document.getElementById('searchInput')
+const searchForm = document.querySelector('#searchForm')
+const linkForm = document.querySelector('#linkForm')
+const taskForm = document.querySelector('#taskForm')
+const searchInput = document.querySelector('#searchInput')
+const linkInput = document.querySelector('#linkInput')
+const taskInput = document.querySelector('#taskInput')
 const searchToggle = document.querySelector('#searchBtn')
-const linksToggle = document.querySelector('#linksBtn')
-const todoToggle = document.querySelector('#todoBtn')
+const linksToggle = document.querySelector('#linkBtn')
+const taskToggle = document.querySelector('#taskBtn')
 const toggleButton = document.querySelectorAll('.svgBtn')
-const todos = document.querySelector('#todos')
+const todos = document.querySelector('.todos')
+
+//Save Tasks in Local Storage
+const LS_TASK_KEY = 'TASKS'
+let saveTask = JSON.parse(localStorage.getItem(LS_TASK_KEY)) || []
+
+//Save Links in Local Storage
+const LS_LINK_KEY = 'LINKS'
+const LS_LINKHEAD_KEY = 'LINKS.SECTION.ID'
+let saveLinks = JSON.parse(localStorage.getItem(LS_LINK_KEY)) || []
+let selectedLinkId = localStorage.getItem(LS_LINKHEAD_KEY)
 
 
 // Clock
@@ -31,92 +37,180 @@ function currentTime() {
 
   let time = `${hh}:${mm} ${ampm}`
   document.getElementById("clock").innerText = time
-  //since there are no seconds being tracked, the clock is only updated every 20sec instead of 1sec for slight performance
+  //since there are no seconds being tracked, the clock is only updated every 20sec instead of 1sec for slight performance increase
   setTimeout(currentTime, 20000)
 }
 
-currentTime() // call
+currentTime()
 
-//using commands such as /r /y /t ...etc the user can search directly on a given site
+
+
+// |-----------------------------------|
+// | * * PAGE AND FORM STATE BEGIN * * | * * * * * * * * * * * * * * *
+// |-----------------------------------|
+
+
+
+// Select a specific button to set it as the currently active, run inputState() to get the function for the current form
+toggleButton.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    toggleButton.forEach((b) => b.classList.remove('active'))
+    btn.classList.add('active')
+    inputState()
+  })
+})
+
+// Save current state of selected button above form and save to localStorage
+// e.g. click Tasks -> add 'active' to Tasks class -> display Tasks form to input data
+function inputState() {
+  switch(true) {
+    // CONTROLS SEARCH TO INTERNET
+    case searchToggle.classList.contains('active'):
+      searchFunc()
+      break;
+
+    // CONTROLS ADDING LINKS TO PAGE
+    case linksToggle.classList.contains('active'):
+      addLinks()
+      break;
+
+    // CONTROLS ADDING TASKS TO TASKLIST
+    case taskToggle.classList.contains('active'):
+      addTasks()
+    break;
+  }
+}
+
+
+
+// |----------------------------|
+// | * * SEARCH STATE BEGIN * * | * * * * * * * * * * * * * * * * * * *
+// |----------------------------|
+
+
+
+// On search form load (inline JS for default behavior), allow input shortcuts to search directly to a site
+// e.g. click Search -> type /y Rick Astley -> you are taken directly to youtube results
 function searchFunc() {
-  const command = search.value.slice(0, 3).toLowerCase()
-  const actionInput = search.value.slice(3)
+  searchForm.style.display = 'block' 
+  linkForm.style.display = 'none'
+  taskForm.style.display = 'none'
+
+  const command = searchInput.value.slice(0, 3).toLowerCase()
+  const actionInput = searchInput.value.slice(3)
   const joinStr = actionInput.split(' ').join('') // make input into one word if it has spaces
 
   switch (command) {
     case '/r ': //reddit
-      form.action = `https://reddit.com/r/${joinStr}`
+      searchForm.action = `https://reddit.com/r/${joinStr}`
+      searchInput.name = ''
       break;
 
     case '/y ': //youtube
-      form.action = `https://www.youtube.com/results?search_query=${actionInput}`
-      form.method = 'POST'
+      searchForm.action = `https://www.youtube.com/results?search_query=${actionInput}`
+      searchForm.method = 'POST'
       break;
 
     case '/t ': //twitter -- search hashtags specifically or just a general search for the keyword
-      form.action = (actionInput.includes('#'))
+      searchForm.action = (actionInput.includes('#'))
                     ? `https://twitter.com/hashtag/${joinStr.slice(1)}` 
                     : `https://twitter.com/search/${actionInput}`
+      searchInput.name = ''
       break;
 
     case '/s ': //stack overflow
-      form.action = `https://stackoverflow.com/search?q=${actionInput}`
-      form.method = 'POST'
+      searchForm.action = `https://stackoverflow.com/search?q=${actionInput}`
+      searchForm.method = 'POST'
+      searchInput.name = ''
       break;
 
     case '/d ': //duck duck go
-      form.action = `https://duckduckgo.com/${actionInput}`
+      searchForm.action = `https://duckduckgo.com/${actionInput}`
+      searchInput.name = ''
       break;
     
     // TODO : allow user to choose their default search
 
     default: // by default we are set to google on the html form
-      form.action = `https://google.com/search?${actionInput}`
-      search.name = 'q'
+      searchForm.action = `https://google.com/search?${actionInput}`
+      searchInput.name = 'q'
   }
 }
 
 
-// |-----------------------------------|
-// | * * PAGE AND FORM STATE BEGIN * * |
-// |-----------------------------------|
 
-// Set active to last clicked button, call inputSaveState() and get data from localStorage
-toggleButton.forEach((btn) => {
-  btn.addEventListener('click', e => {
-    toggleButton.forEach((b) => b.classList.remove('active'))
-    btn.classList.add('active')
-    inputSaveState()
+// |------------------------------|
+// | * * ADD LINKS FORM BEGIN * * | * * * * * * * * * * * * * * * * * *
+// |------------------------------|
+
+
+
+function addLinks() {
+  searchForm.style.display = 'none'
+  linkForm.style.display = 'block'
+  taskForm.style.display = 'none'
+
+  linkForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (linkInput.value === null || linkInput.value === '' || linkInput.value === ' ') return
+    
+    const links = {
+      linkName: linkInput.value,
+      // linkURL: linkUrl(),
+      id: Date.now().toString()
+    }
+    
+    saveLinks.push(links)
+    localStorage.setItem(LS_LINK_KEY, JSON.stringify(saveLinks))
+    console.log(`ADDED ${linkInput.value} TO LINK LOCALSTORAGE`)
+    linkForm.reset()
   })
-})
-
-function inputSaveState() {
-  // Save current state of selected button above form and save to localStorage
-  // e.g. click search -> add 'active' to search class -> change form state to 'search' parameters
-  const LOCAL_STORAGE_SAVESTATE_KEY = 'current.activeForm'
-  let activeForm = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SAVESTATE_KEY)) || []
-
-  switch(true) {
-    // CONTROLS SEARCH TO INTERNET
-    case searchToggle.classList.contains('active'):
-      console.log('search active') // remove later
-      search.placeholder = 'Searchin...'
-      break;
-
-    // CONTROLS ADDING LINKS TO PAGE
-    case linksToggle.classList.contains('active'):
-      console.log('links active') // remove later
-      search.placeholder = 'Add a Link...'
-      break;
-
-    // CONTROLS ADDING TASKS TO TASKLIST
-    case todoToggle.classList.contains('active'):
-      console.log('todo active') // remove later
-      search.placeholder = 'Add a Task...'
-    break;
-  }
 }
 
-// |---------------------------------|
-// | * * PAGE AND FORM STATE END * * |
-// |---------------------------------|
+
+
+// |------------------------------|
+// | * * TASK LIST FORM BEGIN * * | * * * * * * * * * * * * * * * * * *
+// |------------------------------|
+
+
+
+function addTasks() {
+  searchForm.style.display = 'none'
+  linkForm.style.display = 'none'
+  taskForm.style.display = 'block'
+
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (taskInput.value === null || taskInput.value === '' || taskInput.value === ' ') return
+    
+    const task = {
+      taskName: taskInput.value,
+      completed: false,
+      id: Date.now().toString()
+    }
+    
+    saveTask.push(task)
+    localStorage.setItem(LS_TASK_KEY, JSON.stringify(saveTask))
+    console.log(`ADDED ${taskInput.value} TO TASK LOCALSTORAGE`)
+    taskForm.reset() // reset fresh form every submit so user can't spam themselves by holding down enter
+  })
+}
+
+
+
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+
+// finish functions to render tasks from the localstorage
+
+// write function to pull link ids from localstorage and connect them to link headers
+// e.g. new header/div named "Music" -> save a bandcamp link inside the Music div to display in hero/links section
+
+// write a menu for some defaults the user can set
